b7165fe2a733cce8e4481afeaccc54f7f77feefd
Jack_Zhang <zhanghandan0225@outlook.com>
2019-04-01 20:40:33 +0800

    完善编辑环境

---
+++ src/HelloWindow.cpp
@@ -0,0 +1,28 @@
+//
+// Created by zhanghandan on 2019/4/1.
+//
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    return 0;
+}
